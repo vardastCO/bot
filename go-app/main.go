dfd87fb57cdcfc6705578333c8deaf2317a6d3f9
partovi <partovi74@gmail.com>
2023-10-10 10:08:06 -0700

    change

--- go-app/main.go
+++ go-app/main.go
@@ -1,59 +1,187 @@
 package main
 
 import (
+	"bufio"
+	"context"
+	"encoding/json"
+	"fmt"
 	"log"
-    "os/exec"
-    "sync"
+	"os"
+	"os/exec"
+	"sync"
+
+	"github.com/jackc/pgx/v4"
+	"github.com/jackc/pgx/v4/pgxpool"
+)
+
+// Define a struct to represent the scraped data.
+type ScrapeResult struct {
+	URL   string `json:"url"`
+	Price string `json:"price"`
+}
+
+var (
+	dbPool    *pgxpool.Pool
+	csvMutex  sync.Mutex
+	csvFile   *os.File
+	csvWriter *bufio.Writer
+	wg        sync.WaitGroup // Define a WaitGroup for goroutine synchronization
 )
 
 func main() {
-	// Define a list of websites to scrape concurrently.
-	websites := []string{
-		"https://example.com",
-		"https://example2.com",
-		// Add more websites to scrape.
+	// Initialize the database connection pool.
+	dbConnStr := "postgresql://db:root@postgres:5432/db"
+
+	poolConfig, err := pgxpool.ParseConfig(dbConnStr)
+	if err != nil {
+		log.Fatalf("Failed to parse DB connection string: %v", err)
 	}
-
-	// Define the number of worker Goroutines.
-	numWorkers := 100
-
-	// Create a wait group to wait for all Goroutines to finish.
-	var wg sync.WaitGroup
-
-	// Create a channel to distribute the websites among worker Goroutines.
-	websiteChannel := make(chan string, len(websites))
-
-	// Fill the channel with websites.
-	for _, website := range websites {
-		websiteChannel <- website
+	dbPool, err = pgxpool.ConnectConfig(context.Background(), poolConfig)
+	if err != nil {
+		log.Fatalf("Failed to connect to the database: %v", err)
 	}
-	close(websiteChannel) // Close the channel to signal that all websites are enqueued.
+	defer dbPool.Close()
 
-	// Create worker Goroutines.
-	for i := 0; i < numWorkers; i++ {
-		wg.Add(1)
+	// Create the CSV file for writing.
+	createCSVFile()
+	defer closeCSVFile()
 
-		go func() {
-			defer wg.Done()
+	// Start one worker with the initial URL.
+	initialURL := "https://hypersaz.com" // Replace with your initial URL
+	startWorker(initialURL)
 
-			for website := range websiteChannel {
-				// Run the scrape.js file using Node.js.
-				cmd := exec.Command("node", "scrape.js", website)
-
-				// Set the working directory to the location of your scrape.js file.
-				cmd.Dir = "././node-app"
-
-				// Capture the output (stdout and stderr) of the Node.js process.
-				output, err := cmd.CombinedOutput()
-				if err != nil {
-					log.Printf("Error executing scrape.js for %s: %v\n%s", website, err, string(output))
-				} else {
-					log.Printf("Scrape.js executed successfully for %s:\n%s", website, string(output))
-				}
-			}
-		}()
+	// Create additional worker Goroutines.
+	numWorkers := 2
+	for i := 1; i < numWorkers; i++ {
+		wg.Add(1) // Increment the WaitGroup for each worker
+		go func(workerID int) {
+			defer wg.Done()                    // Decrement the WaitGroup when the worker finishes
+			workerURL := getURLToScrape()      // Implement a function to get the next URL to scrape
+			startWorker(workerURL)
+		}(i)
 	}
 
 	// Wait for all worker Goroutines to finish.
 	wg.Wait()
+}
+
+func createCSVFile() {
+	// Open the CSV file for writing.
+	var err error
+	csvFile, err = os.Create("output.csv")
+	if err != nil {
+		log.Printf("Error creating CSV file: %v", err)
+		return
+	}
+
+	csvWriter = bufio.NewWriter(csvFile)
+
+	// Write the CSV header.
+	csvWriter.WriteString("URL,Price\n")
+	csvWriter.Flush()
+}
+
+func closeCSVFile() {
+	if csvFile != nil {
+		csvFile.Close()
+	}
+}
+
+func startWorker(url string) {
+	// Create a worker Goroutine.
+	go func() {
+		defer wg.Done() // Decrement the WaitGroup when the worker finishes
+
+		// Capture the JSON output from scrape.js for this worker.
+		cmd := exec.Command("node", "scrape.js", url)
+		cmd.Env = append(os.Environ(), "WORKER_ID=1") // You can set a unique worker ID here
+
+		// Capture stdout from the Node.js process.
+		output, err := cmd.StdoutPipe()
+		if err != nil {
+			log.Printf("Error getting stdout pipe for worker: %v", err)
+			return
+		}
+
+		// Start the Node.js process.
+		if err := cmd.Start(); err != nil {
+			log.Printf("Error executing scrape.js for worker: %v", err)
+			return
+		}
+
+		// Create a scanner to read the JSON data from stdout.
+		scanner := bufio.NewScanner(output)
+
+		for scanner.Scan() {
+			jsonStr := scanner.Text()
+
+			// Parse JSON data into a struct.
+			var result ScrapeResult
+			if err := json.Unmarshal([]byte(jsonStr), &result); err != nil {
+				log.Printf("Error decoding JSON data from scrape.js for worker: %v", err)
+				continue
+			}
+
+			// Handle the scraped data as needed (e.g., store it in a database).
+			handleScrapedData(result)
+		}
+
+		if err := scanner.Err(); err != nil {
+			log.Printf("Error reading JSON data from scrape.js for worker: %v", err)
+		}
+
+		// Wait for the Node.js process to finish.
+		if err := cmd.Wait(); err != nil {
+			log.Printf("Error waiting for scrape.js for worker: %v", err)
+		}
+	}()
+}
+
+func handleScrapedData(data ScrapeResult) {
+	// Handle the scraped data here, e.g., store it in a database.
+
+	// Write the data to the CSV file.
+	csvMutex.Lock()
+	defer csvMutex.Unlock()
+
+	if csvWriter != nil {
+		// Format the data as a CSV row.
+		csvRow := fmt.Sprintf("%s,%s\n", data.URL, data.Price)
+
+		// Write the CSV row to the file.
+		csvWriter.WriteString(csvRow)
+		csvWriter.Flush()
+	}
+
+	// After processing the data, move the URL to the visited table in the database.
+	markURLAsVisited(data.URL)
+}
+
+func markURLAsVisited(url string) {
+	_, err := dbPool.Exec(context.Background(), "INSERT INTO visited (url) VALUES ($1)", url)
+	if err != nil {
+		log.Printf("Error marking URL as visited: %v", err)
+	}
+}
+
+func getURLToScrape() string {
+	// Implement a function to get the next URL to scrape from the unvisited table.
+	var url string
+	err := dbPool.QueryRow(context.Background(), "SELECT url FROM unvisited LIMIT 1").Scan(&url)
+	if err != nil {
+		if err == pgx.ErrNoRows {
+			// No unvisited URLs left to scrape.
+			return ""
+		}
+		log.Printf("Error getting URL to scrape: %v", err)
+		return ""
+	}
+
+	// Delete the URL from the unvisited table.
+	_, err = dbPool.Exec(context.Background(), "DELETE FROM unvisited WHERE url = $1", url)
+	if err != nil {
+		log.Printf("Error deleting URL from unvisited table: %v", err)
+	}
+
+	return url
 }
--- node-app/scrape.js
+++ node-app/scrape.js
@@ -1,8 +1,7 @@
-const puppeteer = require('puppeteer-core');
-const puppeteerExtra = require('puppeteer-extra');
-const ProxyPlugin = require('puppeteer-extra-plugin-proxy');
+const puppeteer = require('puppeteer');
+const { Client } = require('pg');
+require('dotenv').config();
 
-puppeteerExtra.use(ProxyPlugin());
 const url = process.argv[2]; // Get the URL from the command-line arguments.
 
 if (!url) {
@@ -10,43 +9,117 @@ if (!url) {
     process.exit(1);
 }
 
-(async () => {
-  try {
-    const proxyServer = 'ss://YWVzLTI1Ni1nY206d0dVaGt6WGpjRA==@38.54.13.15:31214#main'; // Replace with your Shadowsocks proxy URL
+const pool = new Client({
+  user: 'db', // PostgreSQL username
+  host: 'postgres', // Use the service name 'postgres' as the host
+  database: 'db', // PostgreSQL database name
+  password: 'root', // PostgreSQL password
+  port: 5432, // PostgreSQL port
+});
 
-    const browser = await puppeteer.launch({
-      headless: false,
-      executablePath: 'C:\\Program Files (x86)\\Google\\Chrome\\Application\\chrome.exe',
-    });
+const initialPage = 'https://www.hypersaz.com/';
+const startUrlPattern = 'https://www.hypersaz.com/product.php?';
 
-    const page = await browser.newPage();
-
-    // Configure the Shadowsocks proxy using the plugin.
-    await page.authenticate({
-      username: proxyServer,
-    });
-
-    // Log a message to indicate that the proxy is being used.
-    // console.log('Using Proxy:', proxyServer);
-    //  await page.goto('https://whatismyipaddress.com/'); // Replace with the URL of the website you want to scrape.
-
-    await page.goto(url); // Replace with the URL of the website you want to scrape.
-    // Use XPath to select the desired element.
-    const xpathExpression = '/html/body/div[1]/div[1]/div[2]/div[4]/div[2]/div[2]/div[2]/div[2]/div[2]/div[2]/ul/li[2]/p';
-    const [element] = await page.$x(xpathExpression);
-
-    if (element) {
-      // Extract the text content of the selected element.
-      const elementText = await page.evaluate(el => el.textContent, element);
-      console.log('Selected element text:', elementText);
-    } else {
-      console.log('Element not found');
+async function createBrowser() {
+    try {
+        const browser = await puppeteer.launch({
+            headless: true,
+        });
+        return browser;
+    } catch (error) {
+        console.error('Error creating the browser:', error);
+        throw error;
     }
+}
 
-    // Close the browser when done.
-    await browser.close();
+const processedHrefs = new Set();
+const unprocessedHrefs = new Set();
+
+async function processPage(pageUrl) {
+  console.log('Processing Page:', pageUrl);
+  try {
+      await pool.connect();
+      const visitedResult = await pool.query('SELECT id FROM visited WHERE url = $1', [pageUrl]);
+
+      if (visitedResult.rows.length === 0) {
+          await pool.query('INSERT INTO visited(url) VALUES($1)', [pageUrl]);
+
+          const page = await browser.newPage();
+          await page.goto(pageUrl, { timeout: 300000 });
+      
+          const priceElement = await page.$x(
+              '/html/body/section[2]/div/div/div[3]/div/ul/li[2]/p/span'
+          );
+          if (priceElement.length > 0) {
+              const priceText = await page.evaluate(
+                  (el) => el.textContent,
+                  priceElement[0]
+              );
+              if (priceText.trim() !== '') {
+            
+                await pool.query('INSERT INTO scraped_data(url, price) VALUES($1, $2)', [pageUrl, priceText.trim()]);
+                console.log(`Saved: URL: ${pageUrl}, Price: ${priceText.trim()}`);
+            }
+          }
+
+          const hrefs = await page.evaluate(() => {
+              const links = Array.from(document.querySelectorAll('a'));
+              return links.map((link) => link.getAttribute('href'));
+          });
+
+          for (const href of hrefs) {
+              if (href && !processedHrefs.has(href)) {
+                  if (!href.startsWith('https://')) {
+                      var outputUrl = initialPage + href;
+                  } else {
+                      var outputUrl = href;
+                  }
+                  if (outputUrl.startsWith(startUrlPattern)) {
+                      // Insert the URL into the 'unvisited' table
+                      await pool.query('INSERT INTO unvisited(url) VALUES($1)', [outputUrl]);
+                      unprocessedHrefs.add(outputUrl);
+                  }
+              }
+          }
+
+          await page.close();
+      }
   } catch (error) {
-    console.error('An error occurred:', error);
+      console.error('An error occurred while navigating to the page:', error);
+      // Handle the error as needed
+  } finally {
+      await pool.end(); // Disconnect from the PostgreSQL database
   }
-})();
+}
 
+
+async function main() {
+    const browser = await createBrowser();
+
+    try {
+        unprocessedHrefs.add(initialPage);
+
+        while (unprocessedHrefs.size > 0) {
+            const currentHref = Array.from(unprocessedHrefs)[0];
+            unprocessedHrefs.delete(currentHref);
+            processedHrefs.add(currentHref);
+
+            await processPage(currentHref);
+        }
+    } catch (error) {
+        console.error('An error occurred:', error);
+    } finally {
+        if (browser) {
+            await browser.close();
+        }
+        console.log('Finished scraping.');
+    }
+}
+
+createBrowser()
+    .then(() => {
+        main();
+    })
+    .catch(() => {
+        console.log('Failed to create a browser.');
+    });
